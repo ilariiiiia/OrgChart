67d978f41eddbe84a172ca3579bf19d485462f27
ilariiiiia <82510639+ilariiiiia@users.noreply.github.com>
2023-05-01 20:43:16 +0000

    Added save and read function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils.treeParser import TreeParser
 from utils.html import renderTribes, choiceHTML
-from utils.save import saveFile
+from utils.save import saveFile, uploadPath, getFileFromId
 
 from flask import Flask, request
 
@@ -22,6 +22,13 @@ def view():
 @app.route('/submitFile', methods=['POST'])
 def upload_file():
 	return saveFile(request, __file__)
+
+@app.route("/test/<path:id>")
+def test_view(id):
+	t = TreeParser(getFileFromId(id))
+	hide = "<script type='text/javascript'>let selected = document.querySelector('select').value;document.querySelectorAll('.allWrapper').forEach((it) => {it.id === selected ? it.style.display = 'flex' : it.style.display = 'none';})</script>"
+	return f"<style>{css}</style>{choiceHTML(t.tribes)}<div class='tribesWrapper'>{renderTribes(t.tribes)}</div>{hide}"
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080, debug=True)
--- utils/save.py
+++ utils/save.py
@@ -1,17 +1,33 @@
 from os.path import join, dirname, realpath
 from werkzeug.utils import secure_filename
 import os
+import json
 
+def uploadPath(_file):
+	return join(dirname(realpath(_file)), 'static/userdata/')
 
 def saveFile(request, _file):
-	UPLOADS_PATH = join(dirname(realpath(_file)), 'static/userdata/')
-	n = len([name for name in os.listdir(UPLOADS_PATH) if os.path.isfile(name)])
+	n = len([name for name in os.listdir(uploadPath(_file)) if os.path.isfile(name)])
 	if request.method == 'POST':
 		if 'file' not in request.files:
 			return "Wrong one little boy"
 		file = request.files['file']
 		if file and file.filename:
 			filename = secure_filename(file.filename)
-			file.save(UPLOADS_PATH + filename)
+			file.save(uploadPath(_file) + filename)
+			saveId(n, uploadPath(_file) + filename)
 			return hex(n + 1234)
 	return '''<h1>Something went wrong!</h1>'''
+
+def getFileFromId(id: int):
+	with open("./static/matches.json", "r") as file:
+		matches = json.load(file)
+		return matches[id]
+
+def saveId(id: int, filename):
+	n = hex(id + 1234)
+	with open("./static/matches.json", "r") as file:
+		matches = json.load(file)
+	matches[n] = filename
+	with open("./static/matches.json", "w") as file:
+		json.dump(matches, file)
